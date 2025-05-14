bb28058b30aee24e4590907e623c3fc8298ec79b
Giddel Chimezuru Ogbonna <134491076+Giddel-Wilson@users.noreply.github.com>
2025-05-14 04:36:26 +0000

    Update database.js

--- src/lib/database.js
+++ src/lib/database.js
@@ -3,6 +3,8 @@ import { PrismaClient } from '@prisma/client';
 // Create a singleton instance of PrismaClient
 const prisma = new PrismaClient();
 
+// Export as both 'prisma' and 'db' to satisfy different import requirements
+export const db = prisma;
 export { prisma };
 
 // Export any other database-related functions or utilities here
